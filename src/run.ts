442f8b646debedc2441c872a0c82fc3cfa655dfe
Doria Keung <doria.keung@gmail.com>
2021-09-08 15:29:53 -0400

    Do not add `bin` to Windows path.

--- src/run.ts
+++ src/run.ts
@@ -55,11 +55,12 @@ async function runSetup(): Promise<null|Error> {
     }
 
     core.info('Adding buf binary to PATH');
-    core.addPath(path.join(installDir, 'bin'));
     let binaryPath = '';
     if (os.platform() === 'win32') {
+      core.addPath(installDir);
       binaryPath = await io.which('buf.exe', true);
     } else {
+      core.addPath(path.join(installDir, 'bin'));
       binaryPath = await io.which('buf', true);
     }
     if (binaryPath === '') {
